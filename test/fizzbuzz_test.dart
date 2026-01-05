d596ed16a0956f24f8bfd62a88a7816090a6873b
Hardik Patel <hardik170594@gmail.com>
2026-01-05 15:43:13 +0530

    test: add test for multiples of 5

--- test/fizzbuzz_test.dart
+++ test/fizzbuzz_test.dart
@@ -7,9 +7,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_fizzbuzz/fizzbuzz_solver.dart';
 
 void main() {
+  late FizzBuzzSolver solver;
+  setUp(() {
+    solver = FizzBuzzSolver();
+  });
+
   test("For multiples of three return 'Fizz' instead of the number 3", () {
     //ARRANGE
-    FizzBuzzSolver solver = FizzBuzzSolver();
     const input = 3;
 
     //ACT
@@ -17,5 +21,16 @@ void main() {
 
     //ASSERT
     expect(output, "Fizz");
+  });
+
+  test("For multiples of five return 'Buzz' for input as 5", () {
+    //ARRANGE
+    const input = 5;
+
+    //ACT
+    final output = solver.solve(input);
+
+    //ASSERT
+    expect(output, "Buzz");
   });
 }
